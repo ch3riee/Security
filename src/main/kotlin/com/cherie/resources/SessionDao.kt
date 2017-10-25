f9d5b1291faeb1f282852445f83f4410c19cbd80
Cherie Huang <cheriemhuang@gmail.com>
2017-10-25 12:10:07 -0700

    Fixed get session bugs and rearranged resource classes

--- src/main/kotlin/com/cherie/resources/SessionDao.kt
+++ src/main/kotlin/com/cherie/resources/SessionDao.kt
@@ -1,38 +1,57 @@
 package com.cherie.resources
 
-import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.BaseJsonNode
+import com.fasterxml.jackson.databind.node.ObjectNode
 import com.jayway.jsonpath.Configuration
 import com.jayway.jsonpath.JsonPath
+import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider
+import javax.annotation.security.RolesAllowed
 import javax.servlet.http.HttpServletRequest
-import javax.ws.rs.*
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
-import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider
-import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider
-import javax.annotation.security.RolesAllowed
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.node.ObjectNode
-import com.mashape.unirest.http.JsonNode
-import org.json.JSONObject
-import javax.print.attribute.standard.Media
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 
 
-@Path("set")
-@RolesAllowed("guest")
-class SetResource{
+@Path("session")
+class SessionDao{
 
+    @GET
+    @Path("get")
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed("guest")
+    fun get( @Context req: HttpServletRequest, @QueryParam("key") key :String? ): Response {
+        val session = req.getSession(false)
+        var theJson = session.getAttribute(key!!.substring(0,1)) as String?
+        if(theJson == null)
+        {
+           return Response.status(Response.Status.NOT_FOUND).type("plain/text").entity("I'm sorry there is no such attribute in the session").build()
+        }
+        val mapper = ObjectMapper()
+        val data =  mapper.readTree(theJson)
+        val configuration = Configuration.builder()
+                .jsonProvider(JacksonJsonNodeJsonProvider())
+                .mappingProvider(JacksonMappingProvider())
+                .build()
+        val path = JsonPath.compile("$" + key!!.substring(1))
+        val newJson: JsonNode = JsonPath.using(configuration).parse(data).read(path)
+       return Response.status(Response.Status.OK).entity(newJson).build()
+
+    }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("set2")
-    fun set2(@QueryParam("key") key :String?, @Context req: HttpServletRequest, data: String?): Response {
+    @Path("set")
+    @RolesAllowed("guest")
+    fun set(@QueryParam("key") key :String?, @Context req: HttpServletRequest, data: String?): Response {
         val session = req.getSession(false)
         val currObj = session.getAttribute(key!!.substring(0,1)) as String?
-        val requestedJSON = JSONObject(data)
 
-        var theroot:ObjectNode? = null
+        var theroot: ObjectNode? = null
         val mapper = ObjectMapper()
         if(currObj == null){
             //must create the entire structure
@@ -47,6 +66,7 @@ class SetResource{
 
         }
         else{
+
             theroot = mapper.readValue(currObj, ObjectNode::class.java)
             val arr = key.split(".")
             var curr: ObjectNode = theroot
@@ -60,26 +80,22 @@ class SetResource{
                 else{
                     curr = temp as ObjectNode
                 }
-                println("the current node that should be created " + curr)
                 i = i + 1
             }
-           // println("This is the whole object " + theroot)
         }
         val configuration = Configuration.builder()
                 .jsonProvider(JacksonJsonNodeJsonProvider())
                 .mappingProvider(JacksonMappingProvider())
                 .build()
         val path = JsonPath.compile("$" + key!!.substring(1))
-       //val testBody = mapper.createObjectNode()
-        //testBody.put("hello", "hi")
-      //  val testBody = mapper.readValue(data, com.fasterxml.jackson.databind.JsonNode::class.java)
         val testBody = mapper.readTree(data)
         val newJson: String? = JsonPath.using(configuration).parse(theroot).set(path, testBody).jsonString()
-        //println("First One: " +newJson)
         session.setAttribute(key!!.substring(0,1),newJson)
-        val a = session.getAttribute(key!!.substring(0,1)) as String
-       // val test = mapper.readValue(session.getAttribute(key!!.substring(0,1)) as String?, ObjectNode::class.java)
+        val a = session.getAttribute(key!!.substring(0,1)) as String //for debugging purposes, can change
         return Response.ok().entity(mapper.readTree(a)).build()
 
     }
+
+
+
 }
