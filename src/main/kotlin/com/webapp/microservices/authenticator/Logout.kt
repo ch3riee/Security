f19bcc405380e959d5f7b331f8c54427c298d19a
Cherie Huang <cheriemhuang@gmail.com>
2018-01-25 18:16:03 -0800

    fixed nginx resolver issue using upstream and cookie deletion in logout

--- src/main/kotlin/com/webapp/microservices/authenticator/Logout.kt
+++ src/main/kotlin/com/webapp/microservices/authenticator/Logout.kt
@@ -13,11 +13,14 @@ class Logout{
 
     @GET
     fun logout(@Context request: HttpServletRequest, @CookieParam("JSESSIONID") cookie: Cookie?): Response {
+
         if(cookie != null)
         {
             request.getSession(false).invalidate()
         }
-        return Response.temporaryRedirect(URI("/rest/public/gateway/session/login")).build()
+        return Response.temporaryRedirect(URI("/rest/public/gateway/session/login"))
+                .header("Set-Cookie", "JwtToken=deleted;Domain=127.0.0.1;Path=/;Expires=Thu, 01-Jan-1970 00:00:01 GMT")
+                .build()
 
     }
 
